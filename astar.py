3d77ec179e18f93d002027e8a970974e49a90847
Clayton Powell <daustoe@gmail.com>
2014-05-23 12:03:35 -0700

    Added comments
    Added comments and modified to make a bit more readable.

--- astar.py
+++ astar.py
@@ -34,51 +34,56 @@ def astar(graph, initial_node, goal_node, h):
     h_score = {initial_node: h(initial_node, goal_node)}  # heuristic estimate
     f_score = {initial_node: h_score[initial_node]}  # estimated distance
 
-    while nodes:
-        x = None
-        for node in nodes:
-            if x is None:
-                x = node
-            elif f_score[node] < f_score[x]:
-                x = node
+    while nodes:  # We pull all nodes in the 'Open Set', nodes that have not been visited
+        current = None
+        for node in nodes:  # Check all nodes for the lowest f_score node (best path)
+            if current is None:
+                current = node  # first iteration through this loop will set current to the first node
+            elif f_score[node] < f_score[current]:  # We compare previous node to the next node and current becomes the smaller of the two
+                current = node
 
-        nodes.remove(x)
-        if x == goal_node:
+        # At this point we have the smallest f_score node (best combined choice of h_score and g_score) in current
+        nodes.remove(current)  # Remove node current from the open_set of unvisited nodes
+        if current == goal_node:  # if current is our goal node we return the set of visited nodes, we are done!
             return visited
 
-        closed_set.add(x)
-        for y in graph.edges[x]:
-            if y in closed_set:
+        closed_set.add(current)  # Add current to the set of closed nodes
+        for neighbor in graph.edges[current]:  # Now we check all neighbors of current
+            if neighbor in closed_set:  # if neighbor is visited (in closed set) we ignore it and continue
                 continue
-            tentative_g_score = g_score[x] + graph.distances[(x, y)]
-            flag = False
-            if y not in nodes or tentative_g_score < g_score[y]:
-                nodes.add(y)
-                flag = True
-            if flag:
-                visited[y] = x
-                g_score[y] = tentative_g_score
-                h_score[y] = h(y, goal_node)
-                f_score[y] = g_score[y] + h_score[y]
-    return False
+            tentative_g_score = g_score[current] + graph.distances[(current, neighbor)]
+            # Calculate g_score (distance known from start to current to this neighbor
+            # If neighbor isn't in the set of Nodes to be evaluated or if it is and this path is lower we add
+            # this neighbor to be evaluated
+            if neighbor not in nodes or tentative_g_score < g_score[neighbor]:
+                nodes.add(neighbor)
+                # add all scoring information about this neighbor so we can determine whether it is optimal
+                # later on
+                visited[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                h_score[neighbor] = h(neighbor, goal_node)
+                f_score[neighbor] = g_score[neighbor] + h_score[neighbor]
+    return False  # Cannot move from start to goal if we return False
 
 
 def shortest_path(graph, initial_node, goal_node, h):
-    paths = astar(graph, initial_node, goal_node, h)
+    paths = astar(graph, initial_node, goal_node, h)  # Returns route taken from astar algorithm
     route = [goal_node]
 
-    while goal_node != initial_node:
-        route.append(paths[goal_node])
+    while goal_node != initial_node:  # Route becomes an array of all Nodes used in the path
+        route.append(paths[goal_node])  # Reverse iterate from goal to start
         goal_node = paths[goal_node]
 
-    route.reverse()
+    route.reverse()  # reverse this list so that we are going from start to goal
     return route
 
 
 if __name__ == '__main__':
     import math
+    # Used Euclidean distance heuristic, slower but a bit more accurate
     sldist = lambda c1, c2: math.sqrt((c2[0] - c1[0])**2 + (c2[1] - c1[1])**2)
     g = Graph()
+    # here we set up the graph we are using for testing purposes
     g.add_node((0, 0))
     g.add_node((1, 1))
     g.add_node((1, 0))
